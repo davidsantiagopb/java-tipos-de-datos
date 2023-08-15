02573265ee9e61061548c8576b0b20d69a09661c
davidsantiagopb <davids.prietob@konradlorenz.edu.co>
2023-08-15 15:35:15 -0500

    Se implementaron las funciones de nódulo y potencia al código previamente establecido.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -8,7 +8,7 @@ public class Calculadora {
         int numero1 = lector.nextInt();
         System.out.println("Ingrese el número 2");
         int numero2 = lector.nextInt();
-        System.out.println("Ingrese la operación (+, -, *, /)");
+        System.out.println("Ingrese la operación (+, -, *, /, %, p");
         char operacion = lector.next().charAt(0);
 
         switch (operacion) {
@@ -33,6 +33,17 @@ public class Calculadora {
                 para que el resultado decimal sea aproximado correctamente. **/
                 double division = (double) numero1 / numero2;
                 System.out.println("La división es " + division);
+            }
+            case '%' -> {
+                // Operación módulo:
+                int módulo= numero1 % numero2;
+                System.out.println("El módulo de la división es " + módulo);
+            }
+            case 'p' -> {
+                // Operación potencia:
+                double exponente = Math.pow (numero1, numero2);
+
+                System.out.println("La potencia del número es " + exponente);
             }
             default -> System.out.println("Operación inválida");
         }
